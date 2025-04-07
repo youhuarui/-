e5780e5edf3502e49e748558d02ff412573a21e4
youhuarui <13252239208@163.com>
2025-04-08 04:28:17 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import os
 base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(
     __name__,
-    template_folder=os.path.join(base_dir, 'templates'),
-    static_folder=os.path.join(base_dir, 'static')
+    template_folder=base_dir,       # 直接指向根目录
+    static_folder=base_dir          # 同样，静态文件也在根目录
 )
 app.register_blueprint(bp)
 
