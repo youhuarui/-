7aa2ce2f8ca37640a2383cf283c1d99b1616f6f4
youhuarui <13252239208@163.com>
2025-04-08 03:44:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from flask import Flask
 from routes import bp
 
 import os
-template_folder = os.path.abspath(os.path.dirname(__file__))
+template_folder = os.path.abspath(os.path.dirname(__file__))  
+static_folder=base_dir
 app = Flask(__name__, template_folder=template_folder)
 app.register_blueprint(bp)
 
